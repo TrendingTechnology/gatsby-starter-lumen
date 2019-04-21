346d67f6ef56a05f739d407c1e86925baa6344a3
Frank He <heshanfu@gmail.com>
2019-04-21 18:21:11 +0800

    Update index-template.js

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
+import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Page from '../components/Page';
 import Pagination from '../components/Pagination';
