228d5c204e812aea787d8a7c116a4cb8aa82c5c2
Frank He <heshanfu@gmail.com>
2019-04-21 18:14:45 +0800

    Update index-template.js

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -1,11 +1,18 @@
+// @flow
 import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
+import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Page from '../components/Page';
 import Pagination from '../components/Pagination';
 
-const IndexTemplate = ({ data, pageContext }) => {
+type Props = {
+  +data: Object,
+  +pageContext: Object,
+};
+
+const IndexTemplate = ({ data, pageContext }: Props) => {
   const {
     title: siteTitle,
     subtitle: siteSubtitle
@@ -24,6 +31,7 @@ const IndexTemplate = ({ data, pageContext }) => {
 
   return (
     <Layout title={pageTitle} description={siteSubtitle}>
+      <Sidebar isIndex />
       <Page>
         <Feed edges={edges} />
         <Pagination
